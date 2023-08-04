aaca0971960109aa63ba1753ac12d56bf3aa20f5
Pedro Perez <pipr96@gmail.com>
2023-08-04 11:02:07 -0700

    add new player form component

---
+++ src/components/AddPuppy.jsx
@@ -0,0 +1,22 @@
+//New player form
+
+
+export default function AddPuppy(){
+
+    return(
+        <form action="">
+            <label htmlFor="">Name:
+                <input type="text" />
+            </label>
+            <label htmlFor="">Breed:
+                <input type="text" />
+            </label>
+            <label htmlFor="">Status
+                <input type="radius" />
+            </label>
+            <label htmlFor="">Team
+                <input type="text" />
+            </label>
+        </form>
+    )
+}
