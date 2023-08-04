0b35979cb1d401ff8f232eea3e5778a2e601e16c
Pedro Perez <pipr96@gmail.com>
2023-08-04 12:49:12 -0700

    add comments

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,27 +1,29 @@
 //gallery of puppy cards
+//import local components
 import SearchBar from './SearchBar'
 import AddPuppy from './AddPuppy'
+//import data
 import fetchPlayers from '../api/index'
-import Card from './Card'
+//import react hooks
 import { useState, useEffect } from 'react'
 
 
-
+//Component for gallery of puppy cards
 export default function Gallery(){  
+    //create a state for players
     const [players, setPlayers] = useState(null);
 
+    //use effect to fetch one time our players
     useEffect(()=>{
-        setPlayers(fetchPlayers());
+        setPlayers(fetchPlayers()); //set players to our data from our function
     },[])
     console.log("players:", players); 
     
-
-
     return(
         <>
-            <SearchBar/>
-            <AddPuppy/>            
-            
+            <SearchBar/>{//add search bar component
+        }   <AddPuppy/> {//add new player form component
+        }           
         </>
     )
 }
