ca238ae600ecdae85bdf5ded39a1e99272140000
Pedro Perez <pipr96@gmail.com>
2023-08-03 20:19:33 -0700

    fetch players from api

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+
+const url = 'https://fsa-puppy-bowl.herokuapp.com/api/2302-acc-pt-d/players';
+
+const fetchPlayers = async () =>{
+    try {
+        const response = await fetch(url);
+        const res = await response.json();
+        const players = res;
+        console.log("players:",res.data.players);
+        return players.data;
+    } catch (error) {
+        console.error
+    }
+}
+
+export default fetchPlayers();
