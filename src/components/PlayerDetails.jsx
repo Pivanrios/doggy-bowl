c9e6c1d999779110aadd1974afa01ec6be2d105d
Pedro Perez <pipr96@gmail.com>
2023-08-04 10:58:49 -0700

    created single player render component

---
+++ src/components/PlayerDetails.jsx
@@ -0,0 +1,20 @@
+//Single player render
+
+export default function PlayerDetails(){
+
+    return(
+        <section>
+            <img src="" alt="" />
+            <ol>
+                <li>name</li>
+                <li>breed</li>
+                <li>status</li>
+                <li>birth</li>
+            </ol>
+            <div>
+                <button>Edit</button>
+                <button>Delete</button>
+            </div>
+        </section>
+    )
+}
