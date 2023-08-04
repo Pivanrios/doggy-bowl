f1fa90cc10e252716a8fa9081efb8d7b6e41d965
Pedro Perez <pipr96@gmail.com>
2023-08-04 12:44:15 -0700

    fetch players and add a state

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,15 +1,27 @@
 //gallery of puppy cards
 import SearchBar from './SearchBar'
 import AddPuppy from './AddPuppy'
+import fetchPlayers from '../api/index'
+import Card from './Card'
+import { useState, useEffect } from 'react'
 
 
-export default function Gallery(){
+
+export default function Gallery(){  
+    const [players, setPlayers] = useState(null);
+
+    useEffect(()=>{
+        setPlayers(fetchPlayers());
+    },[])
+    console.log("players:", players); 
+    
+
 
     return(
-
         <>
             <SearchBar/>
-            <AddPuppy/>
+            <AddPuppy/>            
+            
         </>
     )
 }
