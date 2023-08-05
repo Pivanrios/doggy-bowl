9d33de05218c17ded3df82152ec93765958b0d35
Pedro Perez <pipr96@gmail.com>
2023-08-05 12:22:46 -0700

    change initial state of players, fetch players and map thru them

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,6 +2,7 @@
 //import local components
 import SearchBar from './SearchBar'
 import AddPuppy from './AddPuppy'
+import Card from './Card'
 //import data
 import fetchPlayers from '../api/index'
 //import react hooks
@@ -11,19 +12,27 @@ import { useState, useEffect } from 'react'
 //Component for gallery of puppy cards
 export default function Gallery(){  
     //create a state for players
-    const [players, setPlayers] = useState(null);
+    const [players, setPlayers] = useState([]);
 
     //use effect to fetch one time our players
     useEffect(()=>{
-        setPlayers(fetchPlayers()); //set players to our data from our function
+        const getPlayers = async () =>{
+            try {
+                const puppies = await fetchPlayers();
+                setPlayers(puppies)
+            } catch (error) {
+                console.error
+            }
+        }
+        getPlayers();
     },[])
-    console.log("players:", players); 
+    console.log("players:", players)
     
     return(
         <>
             <SearchBar/>{//add search bar component
         }   <AddPuppy/> {//add new player form component
-        }           
+        }   {players.map(e=>{ return <Card key={e.id} dog={e}/>})}
         </>
     )
 }
