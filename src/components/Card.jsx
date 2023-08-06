3c90ba49f072671811a90e7e0b90bdbe753676eb
Pedro Perez <pipr96@gmail.com>
2023-08-05 23:14:28 -0700

    add status and name to card and set background

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,27 @@
 
 
 export default function Card({key,dog}){
-    
+    //determine color 
+    let color = "";//declare string variable to store our color 
+    if(dog.status == 'field'){//if is active or on field 
+        color = "rgb(17, 219, 3)" //green
+    }else{
+        color="red"
+    }
+
+
     return(
-        <div id="card">
-            <span>{dog.status}</span>
-            <img src={dog.imageUrl} alt="" />
-            <h3>{dog.name}</h3>
+        <div id="card" className="container" style={{
+            backgroundImage:`url(${dog.imageUrl})`,//set background to our img url from the props
+            backgroundSize:'cover',
+        }}>
+            <header id="status" style={{//header of the card
+                backgroundColor:`orange`
+            }}>
+                <div id="status" style={{backgroundColor:`${color}`}}></div>{//depending our condition we set the background color
+            }</header>
+            <h3 className="card">{dog.name}</h3>{//footer of card where we render name
+            }
         </div>
     )
 }
