1149775cdd5ae072e8b000bab1b5ab71fd6171a8
Pedro Perez <pipr96@gmail.com>
2023-08-06 20:06:15 -0700

    add state to form

--- src/components/AddPuppy.jsx
+++ src/components/AddPuppy.jsx
@@ -1,22 +1,62 @@
 //New player form
 
+import { useState } from "react"
+
 
 export default function AddPuppy(){
+    const [userName, setUserName] = useState("");
+    const [breed, setBreed] = useState("");
+    const [status, setStatus] = useState(null);
+    const [imageUrl,setImageUrl] = useState("");
+    const [team, setTeam] = useState("");
 
+    
+    function handleSubmit(){
+
+    }
     return(
-        <form  id={'new-player-form'}>
-            <h3>New Player</h3>
-            <label htmlFor="">Name:
-                <input type="text" />
+        <form  
+            id={'new-player-form'}
+            className='container'
+            action=""
+            onSubmit={handleSubmit}>
+            <h3>Add New Player</h3>
+            <label>Name:{""}
+                <input 
+                    type="text"
+                    value={userName}
+                    onChange={e=> setUserName(e.target.value)}/>
             </label>
-            <label htmlFor="">Breed:
-                <input type="text" />
+            <label>Breed:
+                <input 
+                    type="text"
+                    value={breed}
+                    onChange={e=> setBreed(e.target.value)}/>
             </label>
-            <label htmlFor="">Status
-                <input type="radius" />
+            <label>Status:
+                <span>
+                    <input 
+                        type="radio"
+                        value='field'
+                        name="status"
+                        />field
+                    <input 
+                        type='radio'
+                        value='bench'
+                        name="status"/>bench
+                </span>
             </label>
-            <label htmlFor="">Team
-                <input type="text" />
+            <label>Image:
+                <input 
+                    type="text"
+                    value={imageUrl}
+                    onChange={e=> setImageUrl(e.target.value)}/>
+            </label>
+            <label>Team
+                <input 
+                    type="text"
+                    value={team}
+                    onChange={e=> setTeam(e.target.value)} />
             </label>
             <button>Submit</button>
         </form>
